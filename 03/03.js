c60bc20d736b7f65fc1ac2ae6a3927e108e3ea2b
pnumin <pnumin@pusan.ac.kr>
2024-02-22 12:59:25 +0900

    화살표함수

--- 03/03.js
+++ 03/03.js
@@ -1,9 +1,23 @@
+/* 
+  함수 작성 2 : 화살표 함수
+*/
+const handleClick = (n) => {
+  document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다.</h2>` ;
+}
+
+
+/* 
+  함수 작성 1
+*/
+/*
 function handleClick(n) {
   // 메세지 영역 가져오기 1
   // document.getElementById("msgArea").innerHTML = "안녕하세요" ;
 
   // 메세지 영역 가져오기 2
   // document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>" ;
-  
+
   document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다.</h2>` ;
-}  
+} 
+*/
+
