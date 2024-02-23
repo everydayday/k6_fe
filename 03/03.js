a916f2eb5074224d5eb5d4466b9f4363a113aeac
pnumin <pnumin@pusan.ac.kr>
2024-02-23 15:38:17 +0900

    동적버튼만들기

--- 03/03.js
+++ 03/03.js
@@ -1,10 +1,33 @@
+//DOM tree가 완성된 후 실행
+document.addEventListener("DOMContentLoaded" , () => {
+  console.log("DOMContentLoaded ok") ;
+
+  //버튼생성
+  const bt3 = document.createElement("button") ;
+  const bt3Txt = document.createTextNode("버튼3") ;
+
+  bt3.setAttribute("id", "bt3");
+  bt3.appendChild(bt3Txt) ;
+
+  //버튼추가하기
+  document.querySelector("#btArea2").append(bt3) ;
+
+  //버튼 이벤트 달기
+  bt3.addEventListener("click", ()=>{
+    handleClick(3) ;
+  });
+}) ;
+
+
+
 /* 
   함수 작성 2 : 화살표 함수
 */
 const handleClick = (n) => {
   let msg ;
   if (n == 1) msg = "<h2>안녕하세요.</h2>" ;
-  else msg = "<h2>안녕히 가세요</h2>" ;
+  else if ( n==2 ) msg = "<h2>안녕히 가세요</h2>" ;
+  else msg = `<h2>버튼 ${n} 클릭</h2>`;
 
   document.querySelector("#msgArea").innerHTML = msg ;
 }
